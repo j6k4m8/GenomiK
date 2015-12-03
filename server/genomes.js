d752a25a570225b83f36885b158bdc1fceb3561e
Jordan Matelsky <j6k4m8@gmail.com>
2015-12-02 21:18:13 -0500

    save genomes (add files)

---
+++ server/genomes.js
@@ -0,0 +1,21 @@
+Meteor.methods({
+    _save_genome: function(gen) {
+        /*
+        Accepts a genome document as a parameter. Checks for its
+        validity, and then saves to the database.
+
+        Arguments:
+            gen (Genome): The genome to store
+        */
+
+        if (Genomes.findOne({
+                fileId: gen.fileId
+            })) {
+            return Genomes.update({
+                fileId: gen.fileId
+            }, gen);
+        } else {
+            return Genomes.insert(_.omit(gen, '_id'));
+        }
+    }
+})
