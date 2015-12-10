cce0a314d87da9b2c0e95f933a24ff276f278f52
Jordan Matelsky <j6k4m8@gmail.com>
2015-12-10 13:38:30 -0500

    add docs for router logic

--- iron-genomik/lib/routes.js
+++ iron-genomik/lib/routes.js
@@ -1,37 +1,42 @@
+// Routes for things like the About page
 var infoRoutes = [
     'about',
     'algorithms',
 ];
 
+// Routes that don't require user to be logged in
 var unprotectedRoutes = [
     'home',
 ].concat(infoRoutes);
 
+// Routes that can be simply 'routed' without any
+// extra logic (i.e. not 'Home')
 var simpleRoutes = [
-
 ].concat(infoRoutes);
 
-
+// Configure the routes that don't require login
+// (this is why we made unprotectedRoutes)
 Router.plugin('ensureSignedIn', {
     except: _.pluck(AccountsTemplates.routes, 'name')
              .concat(unprotectedRoutes)
 });
 
+// Configure the router itself
 Router.configure({
     layoutTemplate: 'main'
 });
 
-
+// Add the 'signIn' route.
 AccountsTemplates.configureRoute('signIn');
 
-
+// The default (Home) template
 Router.route('/', function() {
     this.render('home');
 }, {
     name: 'home'
 });
 
-
+// All the simple template names can be routed like this.
 for (var i = 0; i < simpleRoutes.length; i++) {
     Router.route(simpleRoutes[i]);
 }
