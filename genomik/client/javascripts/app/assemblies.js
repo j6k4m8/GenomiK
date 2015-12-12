9e482c35b2f5ca0437beb4e86d749d8118a0c2dd
Jordan Matelsky <j6k4m8@gmail.com>
2015-12-11 23:01:43 -0500

    file extension

--- genomik/client/javascripts/app/assemblies.js
+++ genomik/client/javascripts/app/assemblies.js
@@ -11,4 +11,12 @@ Template.assembly_card.helpers({
         var o = Meteor.users.findOne(this.owner);
         return o.profile.first_name + " " + o.profile.last_name;
     },
+
+    assemblyUrl: function() {
+        if (this.copies.raw_fastas.key.endsWith('.gz')) {
+            return '/assemblies/' + this.copies.raw_fastas.key;
+        } else {
+            return '/assemblies/' + this.copies.raw_fastas.key + '.gz';
+        }
+    }
 });
