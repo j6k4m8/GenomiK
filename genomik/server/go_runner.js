f5cd1a5e309c348e26b174abff19b867098b8da5
Jordan Matelsky <j6k4m8@gmail.com>
2015-12-11 19:36:13 -0500

    add exec fn

---
+++ genomik/server/go_runner.js
@@ -0,0 +1,37 @@
+EXE = "genomik-cli";
+
+stderr = function(args) {
+    console.log(args);
+};
+
+
+runCommand = function(cmd, args, stdout) {
+    /*
+    Run the actual command.
+
+    Arguments:
+        cmd (str): The command to run
+        args (str[]): Arguments
+    */
+
+    // Should do sanitation here first
+    Exec.run(cmd, args, stdout, stderr);
+};
+
+
+Meteor.methods({
+    call_go: function(filename, fileout) {
+        var gzflag = "";
+        if (filename.endsWith('.gz') || filename.endsWith('.gzip')) {
+            gzflag = "--gz";
+        }
+
+        runCommand(EXE, [
+            'unitig',
+            filename,
+            '-o',
+            fileout,
+            gzflag
+        ]);
+    }
+});
