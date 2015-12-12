c3ffe3501e6a393aad828e1739fc4d3e7aef5df2
Jordan Matelsky <j6k4m8@gmail.com>
2015-12-11 22:38:52 -0500

    change args

--- genomik/client/javascripts/app/assemblies.js
+++ genomik/client/javascripts/app/assemblies.js
@@ -10,5 +10,8 @@ Template.assembly_card.helpers({
     ownerName: function() {
         var o = Meteor.users.findOne(this.owner);
         return o.profile.first_name + " " + o.profile.last_name;
+    },
+    rawUrl: function() {
+        this.url();
     }
 });
--- genomik/server/go_runner.js
+++ genomik/server/go_runner.js
@@ -10,14 +10,21 @@ Meteor.methods({
         var gzflag = "";
         if (filename.endsWith('.gz') || filename.endsWith('.gzip')) {
             gzflag = "--gz";
+            Exec.run(EXE, [
+                'unitig',
+                "/home/ubuntu/gk/uploads/raw_fastas/" + filename,
+                '-o',
+                gzflag,
+                fileout,
+            ], console.log, stderr);
+        } else {
+            Exec.run(EXE, [
+                'unitig',
+                '"' + "/home/ubuntu/gk/uploads/raw_fastas/" + filename + '"',
+                '-o',
+                '"' + fileout + '"',
+            ], console.log, stderr);
         }
 
-        Exec.run(EXE, [
-            'unitig',
-            '-o',
-            fileout,
-            "/home/ubuntu/gk/uploads/raw_fastas/" + filename,
-            gzflag,
-        ], console.log, stderr);
     }
 });
