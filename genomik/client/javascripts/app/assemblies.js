4207bae6a502f1fd00176d6e1e54f92d8a524647
Jordan Matelsky <j6k4m8@gmail.com>
2015-12-11 22:41:20 -0500

    link to file url

--- genomik/client/javascripts/app/assemblies.js
+++ genomik/client/javascripts/app/assemblies.js
@@ -11,7 +11,4 @@ Template.assembly_card.helpers({
         var o = Meteor.users.findOne(this.owner);
         return o.profile.first_name + " " + o.profile.last_name;
     },
-    rawUrl: function() {
-        this.url();
-    }
 });
